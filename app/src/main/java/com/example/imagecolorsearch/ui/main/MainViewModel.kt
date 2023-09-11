0492b397cfa534ea4456bac8dafa9cd457fdc57f
dannymcgarvey <dannymcgarvey@verizon.net>
2023-09-10 20:52:58 -0400

    Using redmean distance now. Changed how "require all" works

--- app/src/main/java/com/example/imagecolorsearch/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/imagecolorsearch/ui/main/MainViewModel.kt
@@ -1,7 +1,6 @@
 package com.example.imagecolorsearch.ui.main
 
 import android.app.Application
-import android.graphics.Color
 import android.util.Size
 import androidx.annotation.ColorInt
 import androidx.lifecycle.AndroidViewModel
@@ -10,7 +9,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.cachedIn
 import androidx.paging.filter
-import androidx.palette.graphics.Palette
+import com.example.imagecolorsearch.recycler.ThumbnailData
 import com.example.imagecolorsearch.recycler.ThumbnailDataSource
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.combine
@@ -29,9 +28,9 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
 
     val searchParams = MutableStateFlow(
         SearchParams(
-            filter = listOf(Color.BLUE, Color.RED),
+            filter = emptyList(),
             filterThreshold = 0.5,
-            minimumDensity = 0.2,
+            minimumDensity = 0.5,
             requireAll = false
         )
     )
@@ -41,38 +40,56 @@ class MainViewModel(application: Application) : AndroidViewModel(application) {
             return@combine pagingData
         }
         return@combine pagingData.filter { thumbnailData ->
-            thumbnailData.palette.swatches.any { thumbnailSwatch ->
-                if (params.requireAll) {
-                    params.filter.all { filterColor ->
-                        areColorsWithinThreshold(thumbnailSwatch.rgb, filterColor, params.filterThreshold)
-                                && isSwatchWithinPopulationDensity(thumbnailSwatch, params.minimumDensity)
-                    }
-                } else {
-                    params.filter.any { filterColor ->
-                        areColorsWithinThreshold(thumbnailSwatch.rgb, filterColor, params.filterThreshold)
-                                && isSwatchWithinPopulationDensity(thumbnailSwatch, params.minimumDensity)
-                    }
+            if (params.requireAll) {
+                params.filter.all { filterColor ->
+                    isPopulationAboveMinimum(
+                        getThumbnailColorPopulation(thumbnailData, filterColor, params),
+                        params
+                    )
                 }
+            } else {
+                isPopulationAboveMinimum(
+                    params.filter.fold(0) { acc, filterColor ->
+                        acc + getThumbnailColorPopulation(thumbnailData, filterColor, params)
+                    },
+                    params
+                )
             }
         }
     }
+
+    private fun isPopulationAboveMinimum(population: Int, params: SearchParams) : Boolean {
+        val thumbnailPixelCount = thumbnailSize.height * thumbnailSize.width
+        return population.toDouble() > thumbnailPixelCount * params.minimumDensity
+    }
+
+    private fun getThumbnailColorPopulation(
+        thumbnailData: ThumbnailData,
+        @ColorInt color: Int,
+        searchParams: SearchParams
+    ) = thumbnailData.palette.swatches.filter {
+            areColorsWithinThreshold(color, it.rgb, searchParams.filterThreshold)
+        }.fold(0) { acc, swatch ->
+            acc + swatch.population
+        }
 
     private fun areColorsWithinThreshold(
         @ColorInt color1: Int,
         @ColorInt color2: Int,
         filterThreshold: Double
     ) : Boolean {
+        // https://en.wikipedia.org/wiki/Color_difference
         val r1 = (color1 and 0xFF0000) shr 16
         val g1 = (color1 and 0x00FF00) shr 8
         val b1 = color1 and 0x0000FF
         val r2 = (color2 and 0xFF0000) shr 16
         val g2 = (color2 and 0x00FF00) shr 8
         val b2 = color2 and 0x0000FF
-        val squareDistance = (r1 - r2) * (r1 - r2) + (g1 - g2) * (g1 - g2) + (b1 - b2) * (b1 - b2)
-        return squareDistance.toDouble() / (255 * 255 * 3) < filterThreshold
+        val dr = r1 - r2
+        val dg = g1 - g2
+        val db = b1 - b2
+        val squareDistance = (2.0 + (r1 + r2) / 512.0) * dr * dr + 4 * dg * dg + (2.0 + (510.0 - r1 - r2) / 512.0) * db * db
+        return squareDistance < filterThreshold * filterThreshold * 255 * 255 * 9
     }
-
-    private fun isSwatchWithinPopulationDensity(swatch: Palette.Swatch, minimumDensity: Double) =
-        swatch.population.toDouble() / (thumbnailSize.height * thumbnailSize.width) > minimumDensity
 
 }
